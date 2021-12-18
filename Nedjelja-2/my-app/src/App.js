e4b1d8d4280433d7cf3f33d6aee8484c61dd3f2a
Luka Krsmanović <luka.krsmanovic98@live.ac.me>
2021-12-18 23:13:34 +0100

    Changed class to className

--- Nedjelja-2/my-app/src/App.js
+++ Nedjelja-2/my-app/src/App.js
@@ -3,7 +3,7 @@ import { User } from './User';
 
 function App() {
   return (
-    <div class="ui link cards">
+    <div className="ui link cards">
       <User userInfo={{name: "Luka", job: "Developer"}}></User>
       <User userInfo={{name: "Rade", job: "Electronics"}}></User>
     </div>
--- Nedjelja-2/my-app/src/User.js
+++ Nedjelja-2/my-app/src/User.js
@@ -1,24 +1,24 @@
 export function User(props) {
   return (
-    <div class="card">
-      <div class="image">
+    <div className="card">
+      <div className="image">
         <img src="https://semantic-ui.com/images/avatar2/large/matthew.png" alt="user-image" />
       </div>
-      <div class="content">
-        <div class="header">{props.userInfo.name}</div>
-        <div class="meta">
+      <div className="content">
+        <div className="header">{props.userInfo.name}</div>
+        <div className="meta">
           <a>{props.userInfo.job}</a>
         </div>
-        <div class="description">
+        <div className="description">
           Matthew is an interior designer living in New York.
         </div>
       </div>
-      <div class="extra content">
-        <span class="right floated">
+      <div className="extra content">
+        <span className="right floated">
           Joined in 2013
         </span>
         <span>
-          <i class="user icon"></i>
+          <i className="user icon"></i>
           75 Friends
         </span>
       </div>
