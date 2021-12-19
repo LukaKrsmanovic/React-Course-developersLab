9eda584fde1431c7e5daf46af3b0248679e17faf
Luka Krsmanović <luka.krsmanovic98@live.ac.me>
2021-12-19 19:15:19 +0100

    Used movie API (default 20 items to show), made switching page possible, changed some variable names and style

--- Nedjelja-2/my-app/src/App.js
+++ Nedjelja-2/my-app/src/App.js
@@ -2,34 +2,100 @@ import React from "react";
 import "./App.css";
 import { User } from "./User";
 
+const BASE_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=741a5a16523a2ebd077f3f2a6cba06a2";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       userInfo: { name: "Name", username: "Username: " },
+      allMovies: [],
+      loading: true,
+      currentPage: 1,
     };
   }
 
   componentDidMount() {
-    fetch("https://api.github.com/users/LukaKrsmanovic")
+    fetch(BASE_URL)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        this.setState({ userInfo: data });
+        console.log(data.results);
+        this.setState({ allMovies: data.results, loading: false });
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
-  getUserInfo(name) {
-    return <User userInfo={this.state.userInfo}></User>;
+  getMovieInfo() {
+    let movies = this.state.allMovies;
+    return movies.map((movie) => {
+      return <User movieInfo={movie} key={movie.id}></User>;
+    });
   }
 
+  loadPage = (e) => {
+    let currentPage = this.state.currentPage + 1;
+    if (e.currentTarget.id === "back-button") {
+      currentPage = this.state.currentPage - 1;
+    }
+
+    currentPage = currentPage < 1 ? (currentPage = 1) : currentPage;
+    currentPage = currentPage > 500 ? (currentPage = 500) : currentPage;
+
+    if (currentPage !== this.state.currentPage) {
+      fetch(BASE_URL + "&page=" + currentPage)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({ currentPage, allMovies: data.results });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   render() {
-    return <div className="ui link cards">{this.getUserInfo()}</div>;
+    console.log(this.state.currentPage);
+    if (this.state.loading === true) {
+      return <div class="ui active centered inline loader"></div>;
+    }
+    return (
+      <div id="all">
+        <h1>Popular Movies Page: {this.state.currentPage}</h1>
+        <div className="buttons-container">
+          <div
+            className="ui animated fade button"
+            tabIndex="0"
+            id="back-button"
+            onClick={this.loadPage}
+          >
+            <div className="visible content">Back</div>
+            <div className="hidden content">
+              <i className="left arrow icon"></i>
+            </div>
+          </div>
+          <div
+            className="ui animated fade button"
+            tabIndex="1"
+            id="next-button"
+            onClick={this.loadPage}
+          >
+            <div className="visible content">Next</div>
+            <div className="hidden content">
+              <i className="right arrow icon"></i>
+            </div>
+          </div>
+        </div>
+        <div className="ui link cards">{this.getMovieInfo()}</div>
+      </div>
+    );
   }
 }
 
--- Nedjelja-2/my-app/src/User.js
+++ Nedjelja-2/my-app/src/User.js
@@ -1,23 +1,26 @@
 export function User(props) {
+  const IMG_URL = "https://image.tmdb.org/t/p/w500";
   return (
     <div className="card">
       <div className="image">
-        <img src={props.userInfo.avatar_url} alt="user-image" />
+        <img src={IMG_URL + props.movieInfo.poster_path} alt="user-image" />
       </div>
       <div className="content">
-        <div className="header">{props.userInfo.name}</div>
+        <div className="header">{props.movieInfo.title}</div>
         <div className="meta">
-          <a>Username: {props.userInfo.login}</a>
+          <a>Language: {props.movieInfo.original_language}</a>
         </div>
-        <div className="description">{props.userInfo.bio}</div>
+        <div className="description">
+          {props.movieInfo.overview.slice(0, 150)} ...
+        </div>
       </div>
       <div className="extra content">
         <span className="right floated">
-          Joined in {getDate(props.userInfo.created_at)}
+          Release date: {getDate(props.movieInfo.release_date)}
         </span>
         <span>
-          <i className="user icon"></i>
-          {props.userInfo.followers} Followers
+          <i className="heart outline icon"></i>
+          {props.movieInfo.vote_average * 10}%
         </span>
       </div>
     </div>
